109ef212bc8f0f621742722feb1eb1bb3c6fa307
danish <danish.asytar@megasap.com>
2017-10-09 16:40:18 +0800

    use Area1 model in graph_area1

--- app/admin/graph_area1.rb
+++ app/admin/graph_area1.rb
@@ -1,17 +1,10 @@
-ActiveAdmin.register_page "GraphArea1" do
+ActiveAdmin.register Area1, as: 'GraphArea1' do
 	menu parent: 'Graph', label: 'Graph Area 1' 
 
-	ActiveAdmin.register Area1 do
-		index :title => 'Area 1' do
-			# selectable_column
-			column "ID", :id do |thing|
-			  "AA" + thing.id.to_s.rjust(6, '0')
-			end
-			column "Time In Queue (Seconds)", :time
-			column "Time In", :intime
-			column "Time Out", :outtime
+	index :title => 'Area 1 Chart'
 
-		end	
-	end 
+	
+
+
 
 end
